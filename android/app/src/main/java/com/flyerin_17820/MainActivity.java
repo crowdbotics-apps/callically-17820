8e71933249e58dc9c4f3805e502abda06409d7b8
Crowdbotics <team@crowdbotics.com>
2020-06-04 22:41:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/flyerin_17820/MainActivity.java
@@ -0,0 +1,15 @@
+package com.flyerin_17820;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "flyerin_17820";
+    }
+}
